e8b04e0edf1ffa0c246b04fdc41ecf031d4fa966
Taufik Hidayat <tfkhdyt@proton.me>
2024-11-16 13:08:41 +0700

    chore: setup skeleton ui

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,11 +1,21 @@
+import { skeleton } from '@skeletonlabs/tw-plugin';
+import { join } from 'path';
 import type { Config } from 'tailwindcss';
 
 export default {
-	content: ['./src/**/*.{html,js,svelte,ts}'],
+	darkMode: 'selector',
+	content: [
+		'./src/**/*.{html,js,svelte,ts}',
+		join(require.resolve('@skeletonlabs/skeleton'), '../**/*.{html,js,svelte,ts}')
+	],
 
 	theme: {
 		extend: {}
 	},
 
-	plugins: []
+	plugins: [
+		skeleton({
+			themes: { preset: ['skeleton'] }
+		})
+	]
 } satisfies Config;
