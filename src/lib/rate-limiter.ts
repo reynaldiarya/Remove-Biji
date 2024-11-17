c021b563f196dc28d8216d78c5076eed3303b272
Taufik Hidayat <tfkhdyt@proton.me>
2024-11-17 10:34:48 +0700

    chore(rate-limiter): update rate limit to 3 requests per 5 minutes

--- src/lib/rate-limiter.ts
+++ src/lib/rate-limiter.ts
@@ -2,6 +2,5 @@ import { RateLimiter } from 'sveltekit-rate-limiter/server';
 
 export const limiter = new RateLimiter({
 	// A rate is defined as [number, unit]
-	IP: [4, '10m'], // IP address limiter
-	IPUA: [2, 'm'] // IP + User Agent limiter
+	IPUA: [3, '5m']
 });
