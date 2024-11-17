c33e583cea17d185b2455a4fd7097e2887938bb0
Taufik Hidayat <tfkhdyt@proton.me>
2024-11-17 19:59:08 +0700

    feat(header): adds mobile menu drawer and login snippet

--- src/lib/components/header.svelte
+++ src/lib/components/header.svelte
@@ -1,9 +1,13 @@
 <script lang="ts">
 	import CircleIcon from '$lib/icons/circle-icon.svelte';
+	import CloseIcon from '$lib/icons/close-icon.svelte';
 	import GoogleIcon from '$lib/icons/google-icon.svelte';
+	import HamburgerIcon from '$lib/icons/hamburger-icon.svelte';
 	import { Avatar, LightSwitch } from '@skeletonlabs/skeleton';
 	import { popup } from '@skeletonlabs/skeleton';
 	import type { PopupSettings } from '@skeletonlabs/skeleton';
+	import clsx from 'clsx';
+	import { fade } from 'svelte/transition';
 
 	type Props = {
 		user: {
@@ -24,7 +28,23 @@
 		// Defines which side of your trigger the popup will appear
 		placement: 'bottom'
 	};
+
+	let isOpen = $state(false);
+
+	function handleToggle() {
+		isOpen = !isOpen;
+	}
 </script>
+
+{#snippet loginWithGoogle(className?: string)}
+	<a
+		href="/auth/login/google"
+		class={clsx('plausible-event-name=Login variant-filled-tertiary btn btn-sm', className)}
+	>
+		<GoogleIcon class="mr-2 size-5" />
+		Login with Google
+	</a>
+{/snippet}
 
 <header class="flex items-center justify-between pb-4">
 	<div>
@@ -37,18 +57,18 @@
 		</h1>
 		<h2 class="h6 text-gray-600 dark:text-gray-400">Hilangkan biji mu dengan mudah</h2>
 	</div>
-	<div class="flex items-center space-x-4">
+	<div class="hidden items-center space-x-4 md:flex">
 		<LightSwitch />
 		{#if user}
 			<div use:popup={profilePopup}>
 				<Avatar src={user.picture ?? undefined} width="w-10" rounded="rounded-full" />
 			</div>
 		{:else}
-			<a href="/auth/login/google" class="variant-filled-tertiary btn btn-sm">
-				<GoogleIcon class="mr-2 size-5" />
-				Login with Google
-			</a>
+			{@render loginWithGoogle()}
 		{/if}
+	</div>
+	<div class="md:hidden">
+		<button onclick={handleToggle}> <HamburgerIcon class="size-10" /> </button>
 	</div>
 </header>
 
@@ -59,4 +79,44 @@
 		<a href="/auth/logout" class="variant-ghost-error btn mt-4 w-full">Logout</a>
 	</div>
 	<div class="bg-surface-100-800-token arrow"></div>
+</div>
+
+{#if isOpen}
+	<!-- svelte-ignore a11y_click_events_have_key_events -->
+	<!-- eslint-disable-next-line svelte/valid-compile -->
+	<!-- svelte-ignore a11y_no_static_element_interactions -->
+	<div
+		class="fixed inset-0 z-40 bg-black/35"
+		onclick={handleToggle}
+		transition:fade={{ duration: 300 }}
+	></div>
+{/if}
+
+<div
+	class={clsx(
+		'fixed inset-y-0 right-0 z-50 w-64 space-y-3 bg-surface-100 p-4 shadow-lg transition-all duration-300 dark:bg-surface-900',
+		isOpen ? 'translate-x-0' : 'translate-x-full'
+	)}
+>
+	<button onclick={handleToggle} class="ml-auto"> <CloseIcon class="size-5" /> </button>
+	<div class="w-full space-y-3">
+		<a
+			href="/pricing"
+			class="block rounded px-4 py-2 font-medium hover:bg-surface-300 dark:hover:bg-surface-800"
+			>Pricing</a
+		>
+		<div class="flex items-center justify-between rounded px-4 py-2 font-medium">
+			Theme
+			<LightSwitch />
+		</div>
+		{#if user}
+			<div class="flex items-center justify-between px-4 font-medium">
+				{user.name}
+				<Avatar src={user.picture ?? undefined} width="w-10" rounded="rounded-full" />
+			</div>
+			<a href="/auth/logout" class="variant-ghost-error btn mt-4 w-full">Logout</a>
+		{:else}
+			{@render loginWithGoogle('w-full btn-md')}
+		{/if}
+	</div>
 </div>
---
+++ src/lib/icons/hamburger-icon.svelte
@@ -0,0 +1,15 @@
+<svg
+	xmlns="http://www.w3.org/2000/svg"
+	width="1.2em"
+	height="1.2em"
+	viewBox="0 0 24 24"
+	{...$$props}
+>
+	<path
+		fill="none"
+		stroke="currentColor"
+		stroke-linecap="round"
+		stroke-width="1.5"
+		d="M20 7H4m16 5H4m16 5H4"
+	/>
+</svg>
